633ed2d20be15121b4608695b4ac90f56b560931
Strzaa <strzaa@o2.pl>
2023-10-30 23:24:26 +0100

    Adding prediction

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+podstawa_log = 2
+
 def create_model(X_table, Y_table, rodzaj):
     X_matrix = X_table.to_numpy()
     Y_matrix = Y_table.to_numpy()
@@ -26,18 +28,16 @@ def create_model(X_table, Y_table, rodzaj):
 
     X_matrix_t = np.transpose(X_matrix_1)
 
+    a1, a0 = 0, 0
+
     if rodzaj == "wykladniczy":
-        Y_matrix_log = np.log(Y_matrix) / np.log(math.e)
+        Y_matrix_log = np.log(Y_matrix) / np.log(podstawa_log)
 
         result_matrix = np.dot(np.dot(np.linalg.inv(np.dot(X_matrix_t, X_matrix_1)), X_matrix_t), Y_matrix_log)
 
-        a1, a0 = np.power(math.e, result_matrix[1][0]), np.power(math.e, result_matrix[0][0])
+        a1, a0 = np.power(podstawa_log, result_matrix[1][0]), np.power(podstawa_log, result_matrix[0][0])
 
         print(f"Funkcja wykładnicza y(x) = {a0} + {a1}^x")
-
-        draw(X_matrix, Y_matrix, a1, a0, rodzaj)
-
-        return
 
     elif rodzaj == "liniowy":
         result_matrix = np.dot(np.dot(np.linalg.inv(np.dot(X_matrix_t, X_matrix_1)), X_matrix_t), Y_matrix)
@@ -46,9 +46,9 @@ def create_model(X_table, Y_table, rodzaj):
 
         print(f"Funkcja liniowa y(x) = {a1}*x + {a0}")
 
-        draw(X_matrix, Y_matrix, a1, a0, rodzaj)
+    draw(X_matrix, Y_matrix, a1, a0, rodzaj)
 
-        return
+    return a1, a0
 
 
 def draw(x, y, a1, a0, rodzaj):
@@ -84,8 +84,23 @@ def func_wykl(a1, a0, x):
 
 
 if __name__ == "__main__":
+#   ZAŁADOWANIE EXEL
     exel_data = pd.read_excel("dane.xlsx", sheet_name="Arkusz1")
 
-    create_model(exel_data[["Kwartał"]], exel_data["Liczba uzytkownikow"], "liniowy")
+#   TWORZENIE MODELI
+#   LICZBA_UZ (CZAS) - FUNKCJA LINIOWA
+    a1_lin, a0_lin = create_model(exel_data[["Kwartał"]], exel_data["Liczba uzytkownikow"], "liniowy")
 
-    create_model(exel_data[["Liczba uzytkownikow"]], exel_data["Przychod"], "wykladniczy")
+#   PRZYCHOD (LICZBA_UZ) - FUNKCJA WYKLADNICZA
+    a1_wyk, a0_wyk = create_model(exel_data[["Liczba uzytkownikow"]], exel_data["Przychod"], "wykladniczy")
+
+#   PRZYCHOD POJEDYNCZY
+#   LICZBA UZYTKOWNIKOW W PRZYSZLOSCI DLA KWARTALU 45
+    print(func_liniowa(a1_lin, a0_lin, 45))
+
+#   PRZYCHOD NA PODSTAWIE UZYTKOWNIKÓW DLA LICZBY_UZYTKOWNIKÓW = 2200
+    print(func_wykl(a1_wyk, a0_wyk, 2200))
+
+#   PRZYCHOD ŁĄCZONY
+#   PRZEWIDYWANY PRZYCHOD NA PODSTAWIE CZASU KWARTAL 45
+    print(func_wykl(a1_wyk, a0_wyk, func_liniowa(a1_lin, a0_lin, 35)))
